b04919a8738650ec32929b556075c37e4cc03ba2
Andras Mantia <amantia@kde.org>
2004-09-16 11:22:35 +0000

    Update license & copyright text.
    svn path=/trunk/kdewebdev/kfilereplace/; revision=346919

--- kfilereplace.cpp
+++ kfilereplace.cpp
@@ -1,14 +1,18 @@
-//
-// C++ Implementation: kfilereplace
-//
-// Description:
-//
-//
-// Author:  Andras Mantia <amantia@kde.org>, (C) 2004
-//
-// Copyright: See COPYING file that comes with this distribution
-//
-//
+/***************************************************************************
+               kfilereplace.cpp  -  KFileReplace shell implementation
+                             -------------------
+    begin                : Thu Sep 16 14:14:09 2004
+    copyright            : (C) 2004 by Andras Mantia <amantia@kde.org>
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
 
 //kde includes
 #include <kedittoolbar.h>
--- kfilereplace.h
+++ kfilereplace.h
@@ -1,14 +1,18 @@
-//
-// C++ Interface: kfilereplace
-//
-// Description:
-//
-//
-// Author: Andras Mantia <amantia@kde.org>, (C) 2004
-//
-// Copyright: See COPYING file that comes with this distribution
-//
-//
+/***************************************************************************
+               kfilereplace.h  -  KFileReplace header
+                             -------------------
+    begin                : Thu Sep 16 14:14:09 2004
+    copyright            : (C) 2004 by Andras Mantia <amantia@kde.org>
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
 #ifndef KFILEREPLACE_H
 #define KFILEREPLACE_H
 
@@ -17,7 +21,7 @@
 /**
 Shell application for the KFileReplace part.
 
-@author
+@author Andras Mantia
 */
 class KFileReplace : public KParts::MainWindow
 {
